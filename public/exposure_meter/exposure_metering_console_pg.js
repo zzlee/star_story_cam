a1d43bb79d4589899f88902b7c269a47d8769c27
Gance <gance@feltmeng.com>
2013-10-01 09:40:47 +0800

    Working version with all the parameters set in text input

--- public/exposure_meter/exposure_metering_console_pg.js
+++ public/exposure_meter/exposure_metering_console_pg.js
@@ -3,9 +3,27 @@ $( document ).ready(function() {
     
     $('#btnMeter').click(function(){
         
-        var imageUrl = $("input[type='text'][name='imageUrl']").attr('value');
-        var areaToMeter = JSON.parse($("input[type='text'][name='area']").attr('value'));
-        $('#imageToMeasure').attr('src',imageUrl);
+        var imageUrl = $("input[type='text'][name='imageUrl']").val();
+        var areaToMeter = JSON.parse($("input[type='text'][name='area']").val());
+        //$('#imageToMeasure').attr('src',imageUrl);
+        
+        var sampleImage = null;
+        var previewCanvas = document.getElementById("previewCanvas");
+        var context = previewCanvas.getContext('2d');
+        context.webkitImageSmoothingEnabled = true;
+        sampleImage = document.createElement('img');
+        sampleImage.src = imageUrl;
+        sampleImage.onload = function(){
+            previewCanvas.width = sampleImage.width;
+            previewCanvas.height = sampleImage.height;
+            context.drawImage(sampleImage,0,0);
+            context.beginPath();
+            context.lineWidth="2";
+            context.strokeStyle="red";
+            context.rect(areaToMeter.x, areaToMeter.y, areaToMeter.width, areaToMeter.height);
+            context.stroke();
+        }
+        
 
         
         ExposureMeter.getInstance().getExposureOfArea(imageUrl, areaToMeter, function(errOfGetExposureOfArea, result){
